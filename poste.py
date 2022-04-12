762363529f43f43952dc041b18d44f4ee0e7b270
ralds <ronald.ralds@gmail.com>
2022-04-12 14:37:48 -0300

    criando variaveis da class poste

--- poste.py
+++ poste.py
@@ -1,3 +1,14 @@
 
-class poste:
-    pass
+class Poste:
+    def __init__(self,latitude,longitude,altura,esforco,tipo_de_rede_eletrica,ocupante):
+        print("contruindo objeto")
+        self.latitude = latitude
+        self.longitude = longitude
+        self.altura_do_poste = altura
+        self.esforco_do_poste = esforco
+        self.rede_eletrica = tipo_de_rede_eletrica
+        self.ocupacao = ocupante
+        self.quantidade_de_casa = 0
+        self.quantidade_de_comercio = 0
+        self.quantidade_de_apartamento = 0
+        self.cabo = 0
