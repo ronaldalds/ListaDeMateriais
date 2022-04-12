5daa628a02d9d80aeb9b81536c2f6d29da79589a
ralds <ronald.ralds@gmail.com>
2022-04-12 16:58:13 -0300

    criando variaveis da class cabo

---
+++ cabo.py
@@ -0,0 +1,13 @@
+class Cabo:
+    def __init__(self):
+        self.__percuso = []
+        self.__nome = ""
+        self.__comprimento = 0.0
+        self.__tipo_alca = ""
+        self.__tipo_laco = ""
+        self.__quantidade_de_fibra_no_cabo = 0.0
+        self.__vao_suportado = 0.0
+        self.__peso_do_cabo = 0.0
+        self.diametro_externo = 0.0
+        self.pressao_do_vento = 0.0
+
--- poste.py
+++ poste.py
@@ -1,14 +1,20 @@
 
 class Poste:
-    def __init__(self,latitude,longitude,altura,esforco,tipo_de_rede_eletrica,ocupante):
-        print("contruindo objeto")
-        self.latitude = latitude
-        self.longitude = longitude
-        self.altura_do_poste = altura
-        self.esforco_do_poste = esforco
-        self.rede_eletrica = tipo_de_rede_eletrica
-        self.ocupacao = ocupante
-        self.quantidade_de_casa = 0
-        self.quantidade_de_comercio = 0
-        self.quantidade_de_apartamento = 0
-        self.cabo = 0
+    def __init__(self,tipo_de_poste,latitude,longitude,altura,esforco,tipo_de_rede_eletrica):
+        self.__tipo_de_poste = tipo_de_poste
+        self.__latitude = latitude
+        self.__longitude = longitude
+        self.__altura_do_poste = altura
+        self.__esforco_do_poste = esforco
+        self.__rede_eletrica = tipo_de_rede_eletrica
+        self.__ocupacao = []
+        self.__quantidade_de_casa = 0
+        self.__quantidade_de_comercio = 0
+        self.__quantidade_de_apartamento = 0
+        self.__cabo = []
+        self.__bap = 0
+        self.__sco = 0
+        self.__olhal = 0
+        self.__braco_extensao = 0
+        self.__reserva_tecnica = 0
+        self.__dieletrico = 0
