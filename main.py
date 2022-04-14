4f3aa9cd62fc2aad9d0bd7549f5583392d94a184
ralds <ronald.ralds@gmail.com>
2022-04-14 17:50:12 -0300

    Ajuste em todos os arquivos

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# from poste import Poste
 import xml.etree.ElementTree as Et
 
 doc = Et.parse('Coreaú.kml')
@@ -21,25 +22,21 @@ for i in root.iter('{http://www.opengis.net/kml/2.2}Folder'):
     if 'Poste' in i.findtext('{http://www.opengis.net/kml/2.2}name'):
         for t in i.iter('{http://www.opengis.net/kml/2.2}Placemark'):
             for d in t.iter('{http://www.opengis.net/kml/2.2}Data'):
-                tipo_dado = str(d.findtext('{http://www.opengis.net/kml/2.2}displayName'))
-                dado_poste = str(d.findtext('{http://www.opengis.net/kml/2.2}value'))
-                # if "00" in str(d.findtext('{http://www.opengis.net/kml/2.2}displayName')):
-                #     tipo_poste[numero_poste] = d.findtext('{http://www.opengis.net/kml/2.2}value')
-                # else:
-                #     "tem nao"
-                tipo_poste[numero_poste] = tipo_dado if '00' in dado_poste else "tem nao"
-                latitude = {}
-                longitude = {}
-                altura_poste = {}
-                esforco_poste = {}
-                rede_eletrica = {}
-                quantidade_casa = {}
-                quantidade_comercio = {}
-                quantidade_apartamento = {}
-                tipo_equipamento = {}
-                codigo_poste = {}
-                ocupacao = {}
-                # print(tipo_dado, tipo_poste)
+                valor_poste = d.findtext('{http://www.opengis.net/kml/2.2}value')
+                nome_tipo = str(d.findtext('{http://www.opengis.net/kml/2.2}displayName'))
+
+                if "09" in str(d.findtext('{http://www.opengis.net/kml/2.2}displayName')):
+                    tipo_poste[numero_poste] = d.findtext('{http://www.opengis.net/kml/2.2}value')
+                    break
+                else:
+                    tipo_poste[numero_poste] = "========================"
+
             numero_poste += 1
 
-# print(tipo_poste)
+print(tipo_poste)
+
+# arquivo = Poste('Coreaú.kml')
+#
+#
+# print(arquivo.extracao_tipo_poste)
+# print(arquivo.extracao_coordenada_poste)
--- poste.py
+++ poste.py
@@ -1,62 +1,99 @@
 import xml.etree.ElementTree as Et
 
+
 class Poste:
-    def __init__(self,arquivo):
-        self.__arquivo = arquivo
-        self.__tipo_poste = []
-        self.__latitude = self.extracao_latitude(arquivo)
-        self.__longitude = self.extracao_longitude(arquivo)
-        self.__altura_poste = self.extracao_altura_poste(arquivo)
-        self.__esforco_poste = self.extracao_esforco_poste(arquivo)
-        self.__rede_eletrica = self.extracao_rede_eletrica(arquivo)
-        self.__quantidade_casa = self.extracao_quantidade_casa(arquivo)
-        self.__quantidade_comercio = self.extracao_quantidade_comercio(arquivo)
-        self.__quantidade_apartamento = self.extracao_quantidade_apartamento(arquivo)
-        self.__tipo_equipamento = self.extracao_tipo_equipamento(arquivo)
-        self.__codigo_poste = self.extracao_codigo_poste(arquivo)
-        self.__ocupacao = self.extracao_ocupacao(arquivo)
+    def __init__(self, arquivo):
+        doc = Et.parse(arquivo)
+        self.__root = doc.getroot()
+        self.__coordenada_poste = {}
+        self.__tipo_poste = {}
+        self.__altura_poste = {}
+        self.__esforco_poste = {}
+        self.__rede_eletrica = {}
+        self.__quantidade_casa = {}
+        self.__quantidade_comercio = {}
+        self.__quantidade_apartamento = {}
+        self.__tipo_equipamento = {}
+        self.__codigo_poste = {}
+        self.__ocupacao = {}
+        self.__pasta = '{http://www.opengis.net/kml/2.2}Folder'
+        self.__name = '{http://www.opengis.net/kml/2.2}name'
+        self.__placemark = '{http://www.opengis.net/kml/2.2}Placemark'
+        self.__data = '{http://www.opengis.net/kml/2.2}Data'
+        self.__displayName = '{http://www.opengis.net/kml/2.2}displayName'
+        self.__value = '{http://www.opengis.net/kml/2.2}value'
+        self.__point = '{http://www.opengis.net/kml/2.2}Point'
+        self.__coordinates = '{http://www.opengis.net/kml/2.2}Coordinates'
+        self.__numero_poste = 1
 
 
-    # def __str__(self):
-    #     cliente = self.__quantidade_de_casa + self.__quantidade_de_comercio + self.__quantidade_de_apartamento
-    #     return f"{self.__tipo_de_poste} - {self.__altura_do_poste}/{self.__esforco_do_poste} - C{cliente}"
-    def __getitem__(self, item):
-    def extracao_dados(self):
-        pass
+        for root in self.__root.iter(self.__pasta):
+            if 'Poste' in root.findtext(self.__name):
+                for poste in root.iter(self.__placemark):
+                    for coord in root.iter(self.__point):
+                        self.__coordenada_poste[self.__numero_poste] = coord.findtext(self.__coordinates)
+                    for data in poste.iter(self.__data):
+                        if "00" in str(data.findtext(self.__displayName)):
+                            self.__tipo_poste[self.__numero_poste] = data.findtext(self.__value)
+                            break
+                        else:
+                            self.__tipo_poste[self.__numero_poste] = ""
+
+                        if "01" in str(data.findtext(self.__displayName)):
+                            self.__altura_poste[self.__numero_poste] = data.findtext(self.__value)
+                            break
+                        else:
+                            self.__altura_poste[self.__numero_poste] = ""
+
+                        if "02" in str(data.findtext(self.__displayName)):
+                            self.__esforco_poste[self.__numero_poste] = data.findtext(self.__value)
+                            break
+                        else:
+                            self.__esforco_poste[self.__numero_poste] = ""
+
+                        if "03" in str(data.findtext(self.__displayName)):
+                            self.__rede_eletrica[self.__numero_poste] = data.findtext(self.__value)
+                            break
+                        else:
+                            self.__rede_eletrica[self.__numero_poste] = ""
+
+                        if "04" in str(data.findtext(self.__displayName)):
+                            self.__quantidade_casa[self.__numero_poste] = data.findtext(self.__value)
+                            break
+                        else:
+                            self.__quantidade_casa[self.__numero_poste] = ""
+
+                        if "05" in str(data.findtext(self.__displayName)):
+                            self.__quantidade_comercio[self.__numero_poste] = data.findtext(self.__value)
+                            break
+                        else:
+                            self.__quantidade_comercio[self.__numero_poste] = ""
+
+                        if "06" in str(data.findtext(self.__displayName)):
+                            self.__quantidade_apartamento[self.__numero_poste] = data.findtext(self.__value)
+                            break
+                        else:
+                            self.__quantidade_apartamento[self.__numero_poste] = ""
+
+                        if "07" in str(data.findtext(self.__displayName)):
+                            self.__tipo_equipamento[self.__numero_poste] = data.findtext(self.__value)
+                            break
+                        else:
+                            self.__tipo_equipamento[self.__numero_poste] = ""
+
+                        if "08" in str(data.findtext(self.__displayName)):
+                            self.__codigo_poste[self.__numero_poste] = data.findtext(self.__value)
+                            break
+                        else:
+                            self.__codigo_poste[self.__numero_poste] = ""
+
+                        if "09" in str(data.findtext(self.__displayName)):
+                            self.__ocupacao[self.__numero_poste] = data.findtext(self.__value)
+                            break
+                        else:
+                            self.__ocupacao[self.__numero_poste] = ""
+                    self.__numero_poste += 1
 
 
-
-
-
-
-
-
-
-
-
-
-
-    def extracao_tipo_poste(self):
-        pass
-    def extracao_latitude(self,arquivo):
-        pass
-    def extracao_longitude(self,arquivo):
-        pass
-    def extracao_altura_poste(self,arquivo):
-        pass
-    def extracao_esforco_poste(self,arquivo):
-        pass
-    def extracao_rede_eletrica(self,arquivo):
-        pass
-    def extracao_quantidade_casa(self,arquivo):
-        pass
-    def extracao_quantidade_comercio(self,arquivo):
-        pass
-    def extracao_quantidade_apartamento(self,arquivo):
-        pass
-    def extracao_tipo_equipamento(self,arquivo):
-        pass
-    def extracao_codigo_poste(self,arquivo):
-        pass
-    def extracao_ocupacao(self,arquivo):
-        pass
+arquivo = Poste('Coreaú.kml')
+print(arquivo._Poste__tipo_poste)
