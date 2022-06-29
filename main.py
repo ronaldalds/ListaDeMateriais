d439cafeb1517cfdce210a0bae3cfd44c6d56590
ronaldalds <43586601+ronaldalds@users.noreply.github.com>
2022-06-29 15:04:50 -0300

    deploy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# from flask_wtf.csrf import CSRFProtect
+from flask_wtf.csrf import CSRFProtect
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ app.config.from_pyfile('config.py')
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024  # 10MB max-limit.
 
 db = SQLAlchemy(app)
-# csrf = CSRFProtect(app)
+csrf = CSRFProtect(app)
 bcrypt = Bcrypt(app)
 
 from views import *
