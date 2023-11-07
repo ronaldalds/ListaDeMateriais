4532e19bd0ec37e306121320f138b1a986a3641b
Joao Carlos - PC Online <joao.carlos.dev.log@gmail.com>
2023-11-07 15:38:04 -0300

    Update config.py
    adaptação para execução local

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = 'ruma'
+import os
 
-SQLALCHEMY_DATABASE_URI = \
-    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
-        SGBD='postgresql',
-        usuario='gtxmgfpnwqemej',
-        senha='0a3ade179595158f9d6c9440ed155a467b361e197146ac6af303b234085d1518',
-        servidor='ec2-44-195-162-77.compute-1.amazonaws.com',
-        database='dckbnake75kr9r'
-    )
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = os.getenv('SECRET_KEY', 'ruma')
+
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///database.db')
