63425fa86eda6d0d020f0483d423fab0b3ac89ad
Joao Carlos - PC Online <joao.carlos.dev.log@gmail.com>
2023-11-07 15:37:57 -0300

    Update app.py
    adaptação para execução local

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+
+if os.getenv('FLASK_ENV') == 'production':
+    app.config.from_pyfile('production_config.py')
+else:
+    app.config.from_pyfile('development_config.py')
+
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024  # 10MB max-limit.
 
 db = SQLAlchemy(app)
